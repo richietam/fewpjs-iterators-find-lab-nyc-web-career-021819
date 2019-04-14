28508d5639fab12ad5377051b1e06d8e9286cb3c
richietam <richardtam.ny@gmail.com>
2019-04-14 11:10:22 -0400

    finished lab

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let result = array.find(object => object.result === "W")
+   return !!result ? result.year : undefined
+
+  }
